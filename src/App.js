d72fa8abeccbb3c1fb1f82f8370aa1a148d1e0b9
Marius <marius@example.com>
2022-06-13 09:43:11 +0300

    inline styles simple css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
+  const isLarge = true;
+
+  const btnStyles = {
+    background: 'blue',
+    fontSize: isLarge ? '25px' : '10px',
+  };
+
   return (
     <div className='App'>
+      <Header />
       <h1>Heloo again world</h1>
+      <button style={btnStyles}>Click me</button>
+      <button>Click me no style</button>
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,24 @@
+import './Header.css';
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav className='main-nav'>
+        <a className='nav-link' href='#home'>
+          Home
+        </a>
+        <a className='nav-link' href='#about'>
+          About
+        </a>
+        <a className='nav-link' href='#contact'>
+          Contact
+        </a>
+      </nav>
+      <a className='nav-link search' href='#search'>
+        Search
+      </a>
+    </header>
+  );
 }
 
 export default Header;
