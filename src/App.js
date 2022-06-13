bdba1f3faee3446c587564e922eed021c032bc6f
Marius <marius@example.com>
2022-06-13 10:56:12 +0300

    card module

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Card from './components/Card/Card';
 import Header from './components/header/Header';
 import MyButton from './components/MyButton/MyButton';
 
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
+      <Card />
       <h1>Heloo again world</h1>
       <button style={btnStyles}>Click me</button>
       <button>Click me no style</button>
---
+++ src/components/Card/Card.js
@@ -0,0 +1,24 @@
+import css from './Card.module.css';
+
+function Card() {
+  console.log('css ===', css);
+  return (
+    <div className={css.card}>
+      <img
+        className={css.img}
+        src='https://picsum.photos/id/1025/600/600'
+        alt='card header'
+      />
+      <div className={css.info}>
+        <h3>New York</h3>
+        <p className={css.date}>2022</p>
+        <p className={css.text}>
+          Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales
+          pellentesque elementum.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
